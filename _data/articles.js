72cfc91fc0a01a2a7801ee2e96fa5b4621a17d54
Stephen Barkan <stephen@doist.com>
2024-03-19 13:25:57 -0500

    first commit

---
+++ _data/articles.js
@@ -0,0 +1,70 @@
+module.exports = [
+	{
+		title:
+			"Prominent Logan Square landlord threatens to sue city over zoning change and gets his way—again",
+		link: "https://blockclubchicago.org/2020/12/03/prominent-logan-square-landlord-threatens-to-sue-city-over-zoning-change-and-gets-his-way-again/",
+		source: "Block Club Chicago",
+		date: "December 3, 2020",
+	},
+	{
+		title:
+			"Mark Fishman and Logan Square landlords sue to block affordable housing project near Blue Line",
+		link: "https://blockclubchicago.org/2020/03/03/mark-fishman-and-logan-square-landlords-sue-to-block-affordable-housing-project-near-blue-line/",
+		source: "Block Club Chicago",
+		date: "March 4, 2020",
+	},
+	{
+		title: "Apartment values grow fast in Logan Square",
+		link: "https://www.chicagobusiness.com/residential-real-estate/apartment-values-grow-fast-logan-square-area",
+		source: "Crain’s Chicago Business",
+		date: "July 11, 2019",
+	},
+	{
+		title:
+			"Controversial landlord Mark Fishman gave at least $100,000 to Ald. Ramirez-Rosa's opponent, records show",
+		link: "https://blockclubchicago.org/2019/05/28/controversial-landlord-mark-fishman-gave-at-least-100000-to-ald-ramirez-rosas-opponent-records-show/",
+		source: "Block Club Chicago",
+		date: "May 28, 2019",
+	},
+	{
+		title:
+			"Landlord Mark Fishman kicked me out of my office, Ald. Carlos Ramirez-Rosa says",
+		link: "https://blockclubchicago.org/2019/01/04/mark-fishman-kicked-me-out-of-my-ward-office-ald-carlos-ramirez-rosa-says/",
+		source: "Block Club Chicago",
+		date: "January 4, 2019",
+	},
+	{
+		title:
+			"As developers cash in on Logan Square, one property owner has become the face of gentrification",
+		link: "https://therealdeal.com/chicago/2018/06/08/as-developers-cash-in-on-logan-square-one-property-owner-has-become-the-face-of-gentrification/",
+		source: "The Real Deal",
+		date: "June 8, 2018",
+	},
+	{
+		title:
+			"Landlord M. Fishman's move to evict Logan Square tenants sparks protest",
+		link: "https://www.dnainfo.com/chicago/20171101/logan-square/m-fishman-mark-fishman-eviction-logan-square-protest-wednesday/",
+		source: "DNAInfo Chicago",
+		date: "November 1, 2017",
+	},
+	{
+		title:
+			"As rents skyrocket in Logan Square, landlord M. Fishman defends hikes",
+		link: "https://www.dnainfo.com/chicago/20160325/logan-square/as-rents-skyrocket-logan-square-m-fishman-defends-hikes/",
+		source: "DNAInfo Chicago",
+		date: "March 25, 2016",
+	},
+	{
+		title:
+			"Landlords M. Fishman, Wilmot put ward endorsements under tenant doors",
+		link: "https://www.dnainfo.com/chicago/20150220/logan-square/landlords-m-fishman-wilmot-wedge-ward-endorsements-under-tenant-doors/",
+		source: "Crain’s Chicago Business",
+		date: "February 20, 2015",
+	},
+	{
+		title: "It's Official, M. Fishman Owns Logan Square's Milshire Hotel",
+		link: "https://www.dnainfo.com/chicago/20141218/logan-square/its-official-m-fishman-owns-logan-squares-milshire-hotel/",
+		source: "Block Club Chicago",
+		date: "December 18, 2014",
+	},
+];
---
+++ _data/metadata.js
@@ -0,0 +1,12 @@
+module.exports = {
+	title: "Boycott The Logan Theatre",
+	url: "https://boycottlogantheatre.com/",
+	language: "en",
+	description:
+		"We demand M. Fishman & Co.drop the unjust eviction against our neighbor, Manal Farhan.",
+	author: {
+		name: "Your Name Here",
+		email: "youremailaddress@example.com",
+		url: "https://example.com/about-me/",
+	},
+};
