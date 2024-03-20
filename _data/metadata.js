d956e6b8f7819e62092bf90b60be842cbf8444a5
Stephen Barkan <stephen@doist.com>
2024-03-20 10:38:43 -0500

    social media meta tags

--- _data/metadata.js
+++ _data/metadata.js
@@ -4,6 +4,7 @@ module.exports = {
 	language: "en",
 	description:
 		"We demand M. Fishman & Co.drop the unjust eviction against our neighbor, Manal Farhan.",
+	image: "https://boycottlogantheatre.com/img/og-image.png",
 	author: {
 		name: "Your Name Here",
 		email: "youremailaddress@example.com",
